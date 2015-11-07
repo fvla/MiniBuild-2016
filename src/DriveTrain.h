69579a74de9a48dc4df53f4a188607d723e0a98d
CNTess <cntess1015@gmail.com>
2015-11-07 16:26:25 -0500

    Added DriveTrain class and Constants.h file

---
+++ src/DriveTrain.h
@@ -0,0 +1,22 @@
+/*
+ * DriveTrain.h
+ *
+ *  Created on: Nov 7, 2015
+ *      Author: connortess
+ */
+
+#ifndef SRC_DRIVETRAIN_H_
+#define SRC_DRIVETRAIN_H_
+#include "RobotDrive.h"
+#include "Joystick.h"
+
+class DriveTrain : public RobotDrive {
+
+
+
+public:
+   DriveTrain();
+   virtual ~DriveTrain();
+};
+
+#endif /* SRC_DRIVETRAIN_H_ */
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,49 +1,47 @@
 #include "WPILib.h"
-
+#include "DriveTrain.h"
+#include "Constants.h"
 /**
- * This is a demo program showing how to use Mecanum control with the RobotDrive class.
+ * This is a demo program showing how to use Mecanum control with the driveTrain class.
  */
 class Robot: public SampleRobot
 {
 
-    // Channels for the wheels
-    const static int frontLeftChannel	= 2;
-    const static int rearLeftChannel	= 3;
-    const static int frontRightChannel	= 1;
-    const static int rearRightChannel	= 0;
-
-    const static int joystickChannel	= 0;
-
-	RobotDrive robotDrive;	// robot drive system
-	Joystick stick;			// only joystick
+   // robot drive system
+   DriveTrain driveTrain;
+   // only joystick
+   Joystick stick;
 
 public:
-	Robot() :
-			robotDrive(frontLeftChannel, rearLeftChannel,
-					   frontRightChannel, rearRightChannel),	// these must be initialized in the same order
-			stick(joystickChannel)								// as they are declared above.
-	{
-		robotDrive.SetExpiration(0.1);
-		robotDrive.SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);	// invert the left side motors
-		robotDrive.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);	// you may need to change or remove this to match your robot
-	}
+   Robot() :
+      // these must be initialized in the same order
+      driveTrain(),
+      // as they are declared above.
+      stick(Port::joystickChannel)
+   {
+      driveTrain.SetExpiration(0.1);
+      // invert the left side motors
+      driveTrain.SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);
+      // you may need to change or remove this to match your robot
+      driveTrain.SetInvertedMotor(RobotDrive::kRearLeftMotor, true);
+   }
 
-	/**
-	 * Runs the motors with Mecanum drive.
-	 */
-	void OperatorControl()
-	{
-		robotDrive.SetSafetyEnabled(false);
-		while (IsOperatorControl() && IsEnabled())
-		{
-        	// Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
-        	// This sample does not use field-oriented drive, so the gyro input is set to zero.
-			robotDrive.MecanumDrive_Cartesian(stick.GetX(), stick.GetY(), stick.GetZ());
+   /**
+    * Runs the motors with Mecanum drive.
+    */
+   void OperatorControl()
+   {
+      driveTrain.SetSafetyEnabled(false);
+      while (IsOperatorControl() && IsEnabled())
+      {
+         // Use the joystick X axis for lateral movement, Y axis for forward movement, and Z axis for rotation.
+         // This sample does not use field-oriented drive, so the gyro input is set to zero.
+         driveTrain.MecanumDrive_Cartesian(stick.GetX(), stick.GetY(), stick.GetZ());
 
-			Wait(0.005); // wait 5ms to avoid hogging CPU cycles
-		}
-	}
-
+         // wait 5ms to avoid hogging CPU cycles
+         Wait(0.005);
+      }
+   }
 };
 
 START_ROBOT_CLASS(Robot);
